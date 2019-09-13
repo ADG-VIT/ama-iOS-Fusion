72fa99359f8e67bd7dfa0fec044487210a1f7df5
unknown <uddhav.navneeth@gmail.com>
2019-09-14 01:31:44 +0530

    adding success and negative color change

--- src/components/AMA/ama.js
+++ src/components/AMA/ama.js
@@ -6,30 +6,46 @@ import image from "./adglogo3.png";
 
 const ama = () => {
 
+    const success = () => {
+        document.getElementById("title").style.color="white";
+    }
+
     const submit = async (e) => {
-        e.preventDefault();
-        console.log(document.getElementById("name").value);
-        let info ={
-            "name": document.getElementById("name").value,
-            "regno": document.getElementById("reg").value,
-            "email": document.getElementById("email").value,
-            "message": document.getElementById("doubts").value
+        
+        try {
+            e.preventDefault();
+            console.log(document.getElementById("name").value);
+            let info ={
+                "name": document.getElementById("name").value,
+                "regno": document.getElementById("reg").value,
+                "email": document.getElementById("email").value,
+                "message": document.getElementById("doubts").value
+            }
+
+            let data = await axios.post('https://ama-iosfusion.herokuapp.com/savedata', info);
+            console.log(data);
+
+            document.getElementById("name").value="";
+            document.getElementById("reg").value="";
+            document.getElementById("email").value="";
+            document.getElementById("doubts").value="";
+
+            document.getElementById("title").style.color="rgb(0, 223, 0)";
+            setTimeout(success, 2000);
+        } catch {
+            document.getElementById("title").style.color="red";
+            setTimeout(success, 2000);
         }
+        
 
-        let data = await axios.post('https://ama-iosfusion.herokuapp.com/savedata', info);
-        console.log(data);
-
-        document.getElementById("name").value="";
-        document.getElementById("reg").value="";
-        document.getElementById("email").value="";
-        document.getElementById("doubts").value="";
+        
     } 
 
     return (
         <div class="box">
             <img src={image}></img>
             <div class="askDoubt">
-                <h1 class="title">Ask Your Doubts</h1>
+                <h1 class="title" id="title">Ask Your Doubts</h1>
                     <form>
                         <input class="name" id="name" type="text" placeholder="Your name"></input>
                         <br></br>
