5ed94710c9fb2a15438de41efeeeeb1533a52923
unknown <uddhav.navneeth@gmail.com>
2019-09-15 23:51:14 +0530

    informal git add .

--- src/components/AMA/ama.js
+++ src/components/AMA/ama.js
@@ -17,7 +17,7 @@ const ama = () => {
             console.log(document.getElementById("name").value);
             let info ={
                 "name": document.getElementById("name").value,
-                "regno": document.getElementById("reg").value,
+                "regno": "17SEX0518",
                 "email": document.getElementById("email").value,
                 "message": document.getElementById("doubts").value
             }
@@ -45,15 +45,15 @@ const ama = () => {
         <div class="box">
             <img src={image}></img>
             <div class="askDoubt">
-                <h1 class="title" id="title">Ask Your Doubts</h1>
+                <h1 class="title" id="title">Post Your Message</h1>
                     <form>
-                        <input class="name" id="name" type="text" placeholder="Your name"></input>
+                        <input class="name" id="name" type="text" placeholder="Your name(can be anonymous now)"></input>
                         <br></br>
-                        <input class="name" id="reg" type="text" placeholder="Registration number"></input>
+                        <input class="name" id="reg" type="text" placeholder="Reg No (No need)"></input>
                         <br></br>
-                        <input class="name" id="email" type="text" placeholder="Email ID"></input>
+                        <input class="name" id="email" type="text" placeholder="Email (No Need)"></input>
                         <br></br>
-                        <textarea class="doubt" id="doubts" placeholder="Your Doubts"></textarea>
+                        <textarea class="doubt" id="doubts" placeholder="Your Messages/Feedback"></textarea>
                         <br></br>
                         <button class="button" onClick={(e)=>submit(e)}><Link class="gg" to="/">Send</Link></button>
                     </form>
